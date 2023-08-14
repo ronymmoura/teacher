b39d708ce14eee63634ff61708ca06dd94edc1c4
Rony Moura <ronymmoura@gmail.com>
2023-08-14 18:41:32 -0300

    Remove db access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
-import { PastWord } from "@/components/PastWord";
+// import { PastWord } from "@/components/PastWord";
 import { TeacherChat } from "@/components/TeacherChat";
-import { TodayWord } from "@/components/TodayWord";
-import { WordForm } from "@/components/WordForm";
-import { prisma } from "@/lib/db/prisma";
-import { isSameDay } from "date-fns";
+// import { TodayWord } from "@/components/TodayWord";
+// import { WordForm } from "@/components/WordForm";
+// import { prisma } from "@/lib/db/prisma";
+// import { isSameDay } from "date-fns";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const words = await prisma.word.findMany({ orderBy: { updatedAt: "desc" } });
+  // const words = await prisma.word.findMany({ orderBy: { updatedAt: "desc" } });
 
-  const todayWords = words.filter((word) =>
-    isSameDay(word.updatedAt, new Date())
-  );
-  const pastWords = words.filter(
-    (word) => !isSameDay(word.updatedAt, new Date())
-  );
+  // const todayWords = words.filter((word) =>
+  //   isSameDay(word.updatedAt, new Date())
+  // );
+  // const pastWords = words.filter(
+  //   (word) => !isSameDay(word.updatedAt, new Date())
+  // );
 
   return (
     <>
       <main className="flex h-screen flex-1 flex-col p-3">
         {/* Header */}
-        <WordForm />
+        {/* <WordForm /> */}
 
         {/* Main List */}
-        <div className="flex flex-col space-y-5 overflow-y-auto py-5 scrollbar">
+        {/* <div className="flex flex-col space-y-5 overflow-y-auto py-5 scrollbar">
           {todayWords.map((word) => (
             <TodayWord key={word.id} word={word} />
           ))}
@@ -34,7 +34,7 @@ export default async function Home() {
           {pastWords.map((word) => (
             <PastWord key={word.id} word={word} />
           ))}
-        </div>
+        </div> */}
       </main>
 
       <div className="hidden h-screen flex-col border border-slate-800 bg-zinc-900 bg-opacity-50 shadow-md shadow-black md:flex md:w-[400px]">
